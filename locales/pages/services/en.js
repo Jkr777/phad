0c9d78c89f630e35ea24a2b707d1b4568e86462c
Lucky <janusr777@gmail.com>
2023-09-15 11:17:50 +0300

    u11

---
+++ locales/pages/services/en.js
@@ -0,0 +1,9 @@
+export default {
+  title: "Services",
+
+  p1: "PHAD is a young company founded in 2017 with the mission of supporting enterprise companies with specialist technicians who need to implement ICT solutions aimed at accelerating their business towards their customers and/or users.",
+  p2: "PHAD can offer technical skills with in-depth knowledge of the market in which OEM/Transportation and Utilities companies operate which require specialist support to implement telecommunications systems/infrastructure and innovative PLC/Scada automation solutions to enable IT platforms to make faster decisions relating to their business.",
+  p3: "The specialized consulting services offered by PHAD are intended to be dynamic and scalable and vary from specific short-term projects, to solutions based on performance objectives, up to complete outsourcing (tasks).",
+  p4: "PHAD can rely on professionals with long and proven experience in a vast range of technologies, the result of belonging to multinational groups with a multi-year track record of leadership in the mobile telecommunications sector.",
+  p5: "PHAD can offer specialized consulting in the field of commercial support for the Railway and Telecommunications market, in particular Radio Network Planning, Optimization and Performance Engineering, Quality of Service management, logistics and ITC Solutions."
+}
--- pages/services/index.jsx
+++ pages/services/index.jsx
@@ -1,7 +1,28 @@
+import { useRouter } from 'next/router';
+import { checkLanguage } from "@/utils/utils";
+
+import en from "@/locales/pages/services/en";
+import reClass from "@/styles/reusable.module.css";
+
 function Services() {
+  const router = useRouter();
+
+  const { locale } = router;
+  const txt = checkLanguage(locale, en);
+
   return (
     <section>
-      services
+      <div className={reClass.containerTop}>
+        <h1>{txt.title}</h1>
+      </div>
+
+      <div>
+        <p>{txt.p1}</p>
+        <p>{txt.p2}</p>
+        <p>{txt.p3}</p>
+        <p>{txt.p4}</p>
+        <p>{txt.p5}</p>
+      </div>
     </section>
   );
 }
