0535e1223bf7c16d3abf72cc6e3c90858e1e0688
Lucky <janusr777@gmail.com>
2023-09-18 03:28:54 +0300

    services

--- components/_homepage/services/index.jsx
+++ components/_homepage/services/index.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import classes from "./services.module.css";
 
-import { TbPointFilled } from "react-icons/tb";
+import { AiFillCheckCircle } from "react-icons/ai";
 import { AiFillCloud } from "react-icons/ai";
 import { BsFillPeopleFill } from "react-icons/bs";
 import { IoIosSchool } from "react-icons/io";
@@ -13,20 +13,20 @@ function Services({ txt }) {
 
       <div className={classes.miniContainer}>
       <div className={classes.info}>
-        <span className={`${classes.infoItem} ${classes.big4}`}><TbPointFilled style={{marginRight: "5px", minWidth: "20px"}} />{txt.title1}</span>
+        <span className={`${classes.infoItem} ${classes.big4}`}><AiFillCheckCircle style={{marginRight: "5px", minWidth: "20px"}} />{txt.title1}</span>
         
         <span className={`${classes.infoItem} ${classes.big4}`}><AiFillCloud style={{marginRight: "5px", minWidth: "20px"}} /> {txt.service1}</span>
         
         <span className={`${classes.infoItem} ${classes.big4}`}><BsFillPeopleFill style={{marginRight: "5px", minWidth: "20px"}} />{txt.resources1}</span>  
 
         <span className={`${classes.infoItem} ${classes.big4}`}><IoIosSchool style={{marginRight: "5px", minWidth: "20px"}} />{txt.dip1}</span>
-        {/* <span className={classes.infoItem}><TbPointFilled style={{marginRight: "5px"}} />{txt.option4}</span>
-        <span className={classes.infoItem}><TbPointFilled style={{marginRight: "5px"}} />{txt.option5}</span>
-        <span className={classes.infoItem}><TbPointFilled style={{marginRight: "5px"}} />{txt.option6}</span>
-        <span className={classes.infoItem}><TbPointFilled style={{marginRight: "5px"}} />{txt.option7}</span> */}
+        {/* <span className={classes.infoItem}><AiFillCheckCircle style={{marginRight: "5px"}} />{txt.option4}</span>
+        <span className={classes.infoItem}><AiFillCheckCircle style={{marginRight: "5px"}} />{txt.option5}</span>
+        <span className={classes.infoItem}><AiFillCheckCircle style={{marginRight: "5px"}} />{txt.option6}</span>
+        <span className={classes.infoItem}><AiFillCheckCircle style={{marginRight: "5px"}} />{txt.option7}</span> */}
       </div>
       <div className={classes.info2}>
-        <span className={`${classes.infoItem} ${classes.big4}`}><TbPointFilled style={{marginRight: "5px", minWidth: "20px"}} />{txt.title2}</span>
+        <span className={`${classes.infoItem} ${classes.big4}`}><AiFillCheckCircle style={{marginRight: "5px", minWidth: "20px"}} />{txt.title2}</span>
         
         <span className={`${classes.infoItem} ${classes.big4}`}><AiFillCloud style={{marginRight: "5px", minWidth: "20px"}} /> {txt.service2}</span>
         
