ece6eda97340544895b15e6601865a625541e6b2
Lucky <janusr777@gmail.com>
2023-09-18 14:05:22 +0300

    resource

--- locales/pages/resource/en.js
+++ locales/pages/resource/en.js
@@ -1,3 +1,12 @@
 export default {
-  title: "Resource"
+  title: "Resource",
+
+  projects: "Projects",
+  projName1: "Software optimization of packing lines for Alessi",
+  projName2: "Mapping, migration and development of GUI of SCADA system for Ferrarelle",
+  projName3: "Commercial support for NTT DATA",
+  projName4: "Technical support for physical security systems for TxT Group",
+  projName5: "Support activity on Honolulu metro documentation for NTT DATA",
+  projName6: "Security assessment and cybersecurity with Weseth on Honolulu metro for NTT DATA",
+
 }
--- pages/resource/index.jsx
+++ pages/resource/index.jsx
@@ -1,8 +1,10 @@
 import { useRouter } from 'next/router';
 import { checkLanguage } from "@/utils/utils";
 
+import { MdDone } from "react-icons/md";
 import en from "@/locales/pages/resource/en";
 import reClass from "@/styles/reusable.module.css";
+import classes from "./resource.module.css";
 
 function Resource() {
   const router = useRouter();
@@ -14,6 +16,17 @@ function Resource() {
     <section>
       <div className={reClass.containerTop}>
         <h1>{txt.title}</h1>
+      </div>
+
+      <div className={reClass.sPage}>
+        <h2 className={classes.subTitle}>{txt.projects}</h2>
+
+        <span className={classes.item}><MdDone style={{marginRight: "5px", fontSize: "20px", minWidth: "20px"}}  />{txt.projName1}</span>
+        <span className={classes.item2}><MdDone style={{marginRight: "5px", fontSize: "20px", minWidth: "20px"}}  />{txt.projName2}</span>
+        <span className={classes.item}><MdDone style={{marginRight: "5px", fontSize: "20px", minWidth: "20px"}}  />{txt.projName3}</span>
+        <span className={classes.item2}><MdDone style={{marginRight: "5px", fontSize: "20px", minWidth: "20px"}}  />{txt.projName4}</span>
+        <span className={classes.item}><MdDone style={{marginRight: "5px", fontSize: "20px", minWidth: "20px"}}  />{txt.projName5}</span>
+        <span className={classes.item2}><MdDone style={{marginRight: "5px", fontSize: "20px", minWidth: "20px"}}  />{txt.projName6}</span>
       </div>
     </section>
   );
