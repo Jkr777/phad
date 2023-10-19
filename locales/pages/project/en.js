2f1249266d0c41d081861df23c6c5e24aff1f52d
Lucky <janusr777@gmail.com>
2023-10-19 15:31:38 +0300

    u22

--- locales/pages/project/en.js
+++ locales/pages/project/en.js
@@ -4,23 +4,23 @@ export default {
   projects: "Our Projects",
 
   partenerName1: "NTTDATA",
-  p11:"Dedicate commercial support in the transportation sector;",
-  p12:"Technical support of migration of Scada system on a new HMI platform, with implementation of UX/UI design;",
-  p13:"Engineering support for Passenger Information system for mobility solutions;",
+  p11:"Dedicate commercial support in the transportation sector",
+  p12:"Technical support of migration of Scada system on a new HMI platform, with implementation of UX/UI design",
+  p13:"Engineering support for Passenger Information system for mobility solutions",
   p14:"Abu Dhabi support for L2 access switch.",
 
   partenerName2: "Alessi",
-  p21:"Update of electrical documentation;",
+  p21:"Update of electrical documentation",
   p22:"PLC software optimization and IIOT for packaging lines supported by Engineering Services LD",
 
   partenerName3: "Site S.p.A",
-  p31: "Radio design and radio optimization for Italian mobile operators.",
+  p31: "Radio design and radio optimization for Italian mobile operators",
 
   partenerName4: "TxT Group",
-  p41: "Technical support on Oil and Gas Industries physical security systems.",
+  p41: "Technical support on Oil and Gas Industries physical security systems",
 
   partenerName5: "Consulthink",
-  p51: "Cyber assessment.",
+  p51: "Cyber assessment",
 
   
 }
