70d44515982bbc1f2a86c4595a022568198d30a6
Lucky <janusr777@gmail.com>
2023-11-06 05:12:34 +0200

    close sub

--- components/header/navItems/index.jsx
+++ components/header/navItems/index.jsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 import classes from "./navItems.module.css";
 
-function NavItems({ path, txt, handleClick }) {
+function NavItems({ path, txt, handleClick, closeSubMenu }) {
   return (
     <div className={classes.container}>
       <span href="" onClick={handleClick} className={path === "/services" ? classes['link--active'] : classes.link}>{txt.services}</span>
-      <Link onClick={handleClick} href="/partners" className={path === "/partners" ? classes['link--active'] : classes.link}>{txt.parteners}</Link>
-      <Link onClick={handleClick} href="/project" className={path === "/project" ? classes['link--active'] : classes.link}>{txt.project}</Link>
-      <Link onClick={handleClick} href="/contact" className={path === "/contact" ? classes['link--active'] : classes.link}>{txt.contact}</Link>
+      <Link onClick={closeSubMenu} href="/partners" className={path === "/partners" ? classes['link--active'] : classes.link}>{txt.parteners}</Link>
+      <Link onClick={closeSubMenu} href="/project" className={path === "/project" ? classes['link--active'] : classes.link}>{txt.project}</Link>
+      <Link onClick={closeSubMenu} href="/contact" className={path === "/contact" ? classes['link--active'] : classes.link}>{txt.contact}</Link>
       {/* <span className={classes.contactPhone}>+39 0625399148</span> */}
     </div>
   );
