cb0ece3930d49e0f75414e22e916c2d539591459
Lucky <janusr777@gmail.com>
2023-11-16 02:06:28 +0200

    u1

--- components/header/navItems/index.jsx
+++ components/header/navItems/index.jsx
@@ -7,7 +7,7 @@ import classes from "./navItems.module.css";
 function NavItems({ path, txt, handleClick, closeSubMenu, open }) {
   return (
     <div className={classes.container}>
-      <span href="" onClick={handleClick} className={path === "/services" ? classes['link--active'] : classes.link}>{txt.services}</span>
+      <Link href="/" onClick={handleClick} className={path === "/services" ? classes['link--active'] : classes.link}>{txt.services}</Link>
       <Link onClick={closeSubMenu} href="/partners" className={path === "/partners" && !open ? classes['link--active'] : classes.link}>{txt.parteners}</Link>
       <Link onClick={closeSubMenu} href="/project" className={path === "/project" && !open ? classes['link--active'] : classes.link}>{txt.project}</Link>
       <Link onClick={closeSubMenu} href="/contact" className={path === "/contact" && !open ? classes['link--active'] : classes.link}>{txt.contact}</Link>
