e3e61394fb604aa79db71ac206cd4fd367051e5f
Lucky <janusr777@gmail.com>
2023-11-16 03:12:25 +0200

    u3

--- locales/pages/partners/en.js
+++ locales/pages/partners/en.js
@@ -3,21 +3,21 @@ export default {
   subTitle: "Meet some of our partners",
 
   name: "NTTDATA",
-  info: "Sales and project implementation support.",
+  info: "Sales and project implementation support",
 
-  name2: "TxT",
-  info2: "Technical support on physical security.",
+  name2: "TxT Group",
+  info2: "Technical support on physical security CCTV",
 
   name3: "Consulthink",
-  info3: "Cyber assessment project support.",
+  info3: "Cyber assessment project support",
 
   name4: "Drivesec",
-  info4: "Logical security project support.",
+  info4: "Logical security project support",
 
   name5: "Engineering Services LD",
-  info5: "IIOT project support.",  
+  info5: "IIOT project support",  
   
-  name6: "Alessi",
-  info6: "PLC project implementation.",
+  name6: "Rockwell Automation",
+  info6: "PLC project implementation",
 
 }
--- locales/pages/project/en.js
+++ locales/pages/project/en.js
@@ -5,22 +5,22 @@ export default {
 
   partenerName1: "NTTDATA",
   p11:"Dedicate commercial support in the transportation sector",
-  p12:"Technical support of migration of Scada system on a new HMI platform, with implementation of UX/UI design",
-  p13:"Engineering support for Passenger Information system for mobility solutions",
-  p14:"Abu Dhabi support for L2 access switch",
+  p12:"Technical support for Scada Platform, with implementation of UX/UI design",
+  p13:"Design support on the Passenger Information System for mobility solutions",
+  p14:"Onsite Support on the cybersecurity assessment activity",
 
   partenerName2: "Alessi",
-  p21:"Update of electrical documentation",
-  p22:"PLC software optimization and IIOT for packaging lines supported by Engineering Services LD",
+  p21:"CAD activity",
+  p22:"Automation in logistic, PLC software optimization and IIOT for packaging lines",
 
   partenerName3: "Site S.p.A",
-  p31: "Radio design and radio optimization for Italian mobile operators",
+  p31: "Radio LTE/4G design and radio optimization for Italian mobile operators.",
 
   partenerName4: "TxT Group",
-  p41: "Technical support on Oil and Gas Industries physical security systems",
+  p41: "Design support on the physical security systems for the Oil and Gas Industries.",
 
   partenerName5: "Consulthink",
-  p51: "Cyber assessment",
+  p51: "Cyber assessment automotive",
 
   
 }
